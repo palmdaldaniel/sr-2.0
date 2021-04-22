f8a3ad79e52ea9398ec7dff08b2223c9be5a35e2
daniel <daniel.palmdal@gmail.com>
2021-04-22 15:52:51 +0200

    complete registration functionality

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,34 +1,58 @@
-//const sqlite3 = require('sqlite3');
+const sqlite3 = require('sqlite3');
 const Encrypt = require('../Encrypt')
 const path = require('path');
 
 
 
-//const db = new sqlite3.Database(path.join(__dirname,'../SR_DB.db'))
+const db = new sqlite3.Database(path.join(__dirname, '../SR_DB.db'))
+
 
 const whoami = (req, res) => {
-
     console.log(req.session.user);
-
     res.json(req.session.user || null)
 }
 
 const login = (req, res) => {
     const { email, password  } = req.body
+ /*    
     let rootUser = {
         email: email
+    } */
+    console.log(email);
+
+    let query = /* SQL */ `SELECT * FROM users WHERE email = $email`;
+    let params = {
+        $email: email
     }
+
+    db.get(query, params, (err, user) => {
+         console.log(user);
+       
+        if(user === undefined) {
+            res.status(404).json({err: err, message:'Email or password is incorrect'})
+            return
+        }  
+        console.log(user);
+        req.session.user = user;
+
+        res.json({success: 'logged in', username: user.username})
+    });
+
+
+
+
+/* 
     req.session.user = rootUser
     console.log(req.session.user);
-    res.send({succes: 'logged in', user: rootUser})
+    res.send({succes: 'logged in', user: rootUser}) */
 }
 
 const logout = (req, res) => {
     delete req.session.user
     res.json({succes: 'User is logged out'})
-
-
 }
+
+
 
 module.exports = {
     whoami,
